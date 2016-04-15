28fdc09ad65c82dc5ff99875e4f02fb5351a8442
Alexander Iashchuk <alexander@iashchuk.com>
2016-04-15 15:06:59 +0300

    Initial Commit

---
+++ HOMEPOK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HOMEPOK
+//
+//  Created by Alexander Iashchuk on 4/15/16.
+//  Copyright © 2016 Alexander Iashchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
