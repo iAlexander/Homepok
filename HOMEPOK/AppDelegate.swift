65dd9730780c58d4510f6bdc4e7dd30bd967401e
iAlexander <alexander@iashchuk.com>
2016-08-01 13:46:39 +0300

    Removed pod Firebase dependencies and added the CloudKit support.

--- HOMEPOK/AppDelegate.swift
+++ HOMEPOK/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 		// Override point for customization after application launch.
-		FIRApp.configure()
 		return true
 	}
 
--- Pods/Target Support Files/Pods-HOMEPOKTests/Pods-HOMEPOKTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HOMEPOKTests : NSObject
-@end
-@implementation PodsDummy_Pods_HOMEPOKTests
-@end
