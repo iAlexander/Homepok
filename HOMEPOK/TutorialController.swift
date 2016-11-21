354367ed0887367c59b4e6f909b44be61bb34611
iAlexander <alexander@iashchuk.com>
2016-11-21 22:48:03 +0200

    Added the tutorial screen

---
+++ HOMEPOK/TutorialController.swift
@@ -0,0 +1,35 @@
+//
+//  TutorialController.swift
+//  HOMEPOK
+//
+//  Created by Alexander Iashchuk on 11/21/16.
+//  Copyright © 2016 Alexander Iashchuk. All rights reserved.
+//
+
+import UIKit
+
+class TutorialController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
