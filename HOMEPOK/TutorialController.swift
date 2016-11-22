37a5790f151a07aaef40f682ac0d710a2e256870
iAlexander <alexander@iashchuk.com>
2016-11-22 23:27:49 +0200

    Added one more step to the tutorial

--- HOMEPOK/TutorialController.swift
+++ HOMEPOK/TutorialController.swift
@@ -16,13 +16,13 @@ class TutorialController: UIViewController {
     @IBOutlet weak var tutorialButton: UIButton!
     @IBOutlet weak var finishButton: UIButton!
     
-    var tutorialSteps = 1
+    var tutorialSteps = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         finishButton.isHidden = true
-        topTextLabel.text = "Для того щоб перевірити номер"
-        bottomTextLabel.text = "потрібно тапнути на перші дві літери"
+        topTextLabel.text = "Привіт, здогадуюсь що ви тут вперше!"
+        bottomTextLabel.text = "Тож пропоную пройти коротке навчання"
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,10 +31,14 @@ class TutorialController: UIViewController {
     
     @IBAction func nextButton(_ sender: Any) {
         tutorialSteps += 1
-        if tutorialSteps == 2 {
+        if tutorialSteps == 1 {
+            tutorialImageView.image = UIImage(named: "Tutorial1")
+            topTextLabel.text = "Для того щоб перевірити номер"
+            bottomTextLabel.text = "потрібно тапнути на перші дві літери"
+        } else if tutorialSteps == 2 {
             tutorialImageView.image = UIImage(named: "Tutorial2")
-            topTextLabel.text = "Тепер введіть перші дві літери"
-            bottomTextLabel.text = "й тапніть на кнопку \"Пошук\""
+            topTextLabel.text = "Ввести ці літери з клавіатури"
+            bottomTextLabel.text = "а після тапнути на кнопку \"Пошук\""
         } else if tutorialSteps == 3 {
             tutorialImageView.image = UIImage(named: "Tutorial3")
             topTextLabel.text = "Вітаємо, ви закінчили навчання!"
